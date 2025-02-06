f84ede79a6190c294be4044f6cb7d661a96c9ea7
Quateem Gaffney-Smith <90237467+qgaffney@users.noreply.github.com>
2025-02-06 16:32:48 -0500

    Forgot to push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import DetailedOrder from "/Users/q.gaffney/Desktop/CMSC300/Assignments/uber-eats-dashboard/src/modules/DetailedOrder/index.js";
+import Orders from "qgaffney/uber-eats-dashboard/src/modules/Orders";
 
 function App() {
   return (
-    <DetailedOrder />
+    <Orders />
   );
-}
+};
 
 export default App;
