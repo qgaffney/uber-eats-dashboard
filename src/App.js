69842961a4e95ac5aebb70f6a35f747b9d7526f3
Quateem Gaffney-Smith <90237467+qgaffney@users.noreply.github.com>
2025-02-12 21:04:19 -0500

    Create orders details page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import DetailedOrder from "/Users/q.gaffney/Desktop/CMSC300/Assignments/uber-eats-dashboard/src/modules/DetailedOrder/index.js";
-import Orders from "qgaffney/uber-eats-dashboard/src/modules/Orders";
+import Orders from "qgaffney/uber-eats-dashboard/src/assets/data/dashboard/orders.json";
 
 function App() {
   return (
